156f02d89e3d50a71f4c41faf40a880814261d3b
Randy-jun <275212517@qq.com>
2017-04-23 21:59:56 +0800

    OK

---
+++ autoscript.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import sys
+print("dsads")
--- autoscript.sh
+++ autoscript.sh
@@ -1,8 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-#This is my autoconfig tools.
-echo "This is my autoconfig tools."
-echo "It's working..."
-DATE = 'date + %Y:%m:%d'
-echo $DATE
-#var os_type = 
+DATE=$(date)
+echo "Time is $DATE"
+
+#sudo apt update && sudo apt -y upgrade
+#sudo apt -y install htop vim tmux bash
+
+ln -s $HOME/myautoconfig/dotfiles/tmux.conf $HOME/myautoconfig/.tmux.conf 2> /dev/null
+if [ $? = 0 ] ; then
+	echo -e "Tmux deployed [\033[;32mFinish\033[;m]."
+else
+	echo -e "Tmux deployed [\033[;31mFaile\033[;m]."
+fi
+
+ln -s $HOME/myautoconfig/dotfiles/vimrc $HOME/myautoconfig/.vimrc 2> /dev/null
+if [ $? = 0 ] ; then
+	echo -e "Vim deployed [\033[;32mFinish\033[;m]."
+else
+	echo -e "Vim deployed [\033[;31mFaile\033[;m]."
+fi
+
+ln -s $HOME/myautoconfig/dotfiles/zshrc $HOME/myautoconfig/.zshrc 2> /dev/null
+if [ $? = 0 ] ; then
+	echo -e "Zsh deployed [\033[;32mFinish\033[;m]."
+else
+	echo -e "Zsh deployed [\033[;31mFaile\033[;m]."
+fi
+
+#ln -s $HOME/myautoconfig/dotfiles/vimrc $HOME/myautoconfig/.vimrc
+#ln -s $HOME/myautoconfig/dotfiles/zshrc $HOME/myautoconfig/.zshrc
