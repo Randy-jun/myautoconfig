86d7061907d8d01f72875f08ae962f90cccee740
Randy-jun <275212517@qq.com>
2017-04-24 10:32:31 +0800

    refine

--- autoscript.sh
+++ autoscript.sh
@@ -6,9 +6,9 @@ echo "Time is $DATE"
 #sudo apt update && sudo apt -y upgrade
 sudo apt -y install htop vim tmux zsh > /dev/null 2>&1
 if [ $? = 0 ] ; then
-	echo -e "Software has been installed [\033[;32mFinish\033[;m."
+	echo -e "Software has been installed [\033[;32mFinish\033[;m]."
 else
-	echo -e "Software has been installed [\033[;32mFaile\033[;m."
+	echo -e "Software has been installed [\033[;31mFaile\033[;m]."
 fi
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm > /dev/null 2>&1
@@ -26,7 +26,7 @@ else
 	echo -e "Vim deployed [\033[;31mFaile\033[;m]."
 fi
 
-cd $HOME && curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh > /dev/null 2>&1 && cd -
+cd $HOME && curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh > /dev/null 2>&1 && cd - >/dev/null 2>&1
 ln -s $HOME/myautoconfig/dotfiles/zshrc $HOME/.zshrc > /dev/null 2>&1
 if [ $? = 0 ] ; then
 	echo -e "Zsh deployed [\033[;32mFinish\033[;m]."
