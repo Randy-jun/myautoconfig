297ce16c6e138e542a8a7e77fb8c2bd6a0b58951
Randy-jun <275212517@qq.com>
2017-04-24 09:57:47 +0800

    refine

--- autoscript.sh
+++ autoscript.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 DATE=$(date)
 echo "Time is $DATE"
@@ -6,23 +6,23 @@ echo "Time is $DATE"
 #sudo apt update && sudo apt -y upgrade
 sudo apt -y install htop vim tmux zsh
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 2> /dev/null
-ln -s $HOME/myautoconfig/dotfiles/tmux.conf $HOME/.tmux.conf 2> /dev/null
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm > /dev/null 2> &1
+ln -s $HOME/myautoconfig/dotfiles/tmux.conf $HOME/.tmux.conf > /dev/null 2> &1
 if [ $? = 0 ] ; then
 	echo "Tmux deployed [\033[;32mFinish\033[;m]."
 else
 	echo "Tmux deployed [\033[;31mFaile\033[;m]."
 fi
 
-ln -s $HOME/myautoconfig/dotfiles/vimrc $HOME/.vimrc 2> /dev/null
+ln -s $HOME/myautoconfig/dotfiles/vimrc $HOME/.vimrc > /dev/null 2> &1
 if [ $? = 0 ] ; then
 	echo "Vim deployed [\033[;32mFinish\033[;m]."
 else
 	echo "Vim deployed [\033[;31mFaile\033[;m]."
 fi
 
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh 2> /dev/null
-ln -s $HOME/myautoconfig/dotfiles/zshrc $HOME/.zshrc 2> /dev/null
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh > /dev/null 2> &1
+ln -s $HOME/myautoconfig/dotfiles/zshrc $HOME/.zshrc > /dev/null 2> &1
 if [ $? = 0 ] ; then
 	echo "Zsh deployed [\033[;32mFinish\033[;m]."
 else
