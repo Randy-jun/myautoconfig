af1a215ce4ca031b1dff966ee8e8dfd72440ac13
Randy-jun <275212517@qq.com>
2017-04-25 12:49:01 +0800

    refine

--- autoscript.sh
+++ autoscript.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
-oh_my_zsh_install()
+oh_my_zsh_config()
 {
-	#sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
-	git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh/ > /dev/null 2>&1;
+	sudo chsh $LOGNAME -s /bin/zsh > /dev/null 2>&1 &&
+	git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh/ > /dev/null 2>&1 &&
+	ln -s $HOME/myautoconfig/dotfiles/zshrc $HOME/.zshrc > /dev/null 2>&1;
+	if [ $? = 0 ] ; then
+		exit 0;
+	else
+		exit 1;
+	fi
+}
+
+tmux_config()
+{
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm > /dev/null 2>&1 &&
+	ln -s $HOME/myautoconfig/dotfiles/tmux.conf $HOME/.tmux.conf > /dev/null 2>&1 &&
+	ch -c $HOME/.tmux/plugins/tpm/tpm > /dev/null 2>&1;
+	if [ $? = 0 ] ; then
+		exit 0;
+	else
+		exit 1;
+	fi
+}
+
+vim_config()
+{
+	ln -s $HOME/myautoconfig/dotfiles/vimrc $HOME/.vimrc > /dev/null 2>&1;
+	if [ $? = 0 ] ; then
+		exit 0;
+	else
+		exit 1;
+	fi
 }
 
 main()
 {
 	#sudo apt update && sudo apt -y upgrade;
-	sudo apt -y install htop vim tmux zsh > /dev/null 2>&1 &&
+	sudo apt -y install htop vim tmux zsh > /dev/null 2>&1;
 
 	if [ $? = 0 ] ; then
 		echo -e "Software has been installed [\033[;32mFinish\033[;m].";
@@ -17,23 +45,21 @@ main()
 		echo -e "Software has been installed [\033[;31mFaile\033[;m].";
 	fi
 
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm > /dev/null 2>&1 &&
-	ln -s $HOME/myautoconfig/dotfiles/tmux.conf $HOME/.tmux.conf > /dev/null 2>&1;
+	tmux_config;
 	if [ $? = 0 ] ; then
 		echo -e "Tmux deployed [\033[;32mFinish\033[;m].";
 	else
 		echo -e "Tmux deployed [\033[;31mFaile\033[;m].";
 	fi
 
-	ln -s $HOME/myautoconfig/dotfiles/vimrc $HOME/.vimrc > /dev/null 2>&1;
+	vim_config;
 	if [ $? = 0 ] ; then
 		echo -e "Vim deployed [\033[;32mFinish\033[;m].";
 	else
 		echo -e "Vim deployed [\033[;31mFaile\033[;m].";
 	fi
 
-	sudo chsh $LOGNAME -s /bin/zsh > /dev/null 2>&1 &&
-	oh_my_zsh_install && ln -s $HOME/myautoconfig/dotfiles/zshrc $HOME/.zshrc > /dev/null 2>&1;
+	oh_my_zsh_install;
 	if [ $? = 0 ] ; then
 		echo -e "Zsh deployed [\033[;32mFinish\033[;m].";
 	else
@@ -43,7 +69,9 @@ main()
 
 update()
 {
-	echo "$0 update";
+	cd $HOME/myautoconfig/;
+	git pull;
+	exit 0;
 }
 
 DATE=$(date);
@@ -57,14 +85,16 @@ if [ $# -ge 0 -a $# -le 1 ]; then
 	fi
 	case $1 in
 		install) echo "$0 install start..." && main;
-			exit 0
+			exit 0;
 			;;
 		update) echo "$0 update start..." && update;
-			exit 0
+			exit 0;
 			;;
 		*) echo -e "Usage:$0 1[ \033[;32minstall\033[;m | \033[;32mupdate\033[;m ]";
+			exit 0;
 			;;
 	esac
 else
 	echo -e "Usage:$0 [ \033[;32minstall\033[;m | \033[;32mupdate\033[;m ]";
+	exit 0;
 fi
