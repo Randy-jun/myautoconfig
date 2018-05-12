32bdab9079f17aea100f9b546bd5a32eaadf8c7f
Randy-jun <275212517@qq.com>
2018-05-12 15:57:11 +0800

    modify

--- host_wrtie.sh
+++ host_wrtie.sh
@@ -1,2 +1,3 @@
-echo "127.0.0.1 tp5.localhost" >> /etc/hosts
-echo "127.0.0.1 phpmyadmin.localhost" >> /etc/hosts
+echo "127.0.0.1 tp5.com" >> /etc/hosts
+echo "127.0.0.1 phpmyadmin.com" >> /etc/hosts
+echo "127.0.0.1 test.com" >> /etc/hosts
