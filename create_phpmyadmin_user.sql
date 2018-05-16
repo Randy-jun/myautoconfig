137330b46a67a85a0b5a9cb20369088e0fdef0b4
Randy-jun <275212517@qq.com>
2018-05-16 11:17:24 +0800

    modify

---
+++ create_phpmyadmin_user.sql
@@ -0,0 +1,13 @@
+/**********Allow access from localhost********/
+use mysql;
+CREATE USER 'phpmyadmin'@'localhost' IDENTIFIED BY 'mysql_passwd';
+GRANT ALL PRIVILEGES ON \*.\* TO 'phpmyadmin'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+/******************************************/
+
+/**********Allow access from remote********
+* use mysql;
+* CREATE USER 'phpmyadmin'@'localhost' IDENTIFIED BY 'mysql_passwd';
+* GRANT ALL PRIVILEGES ON \*.\* TO 'phpmyadmin'@'localhost' WITH GRANT OPTION;
+* FLUSH PRIVILEGES;
+*/
--- mariadb.sh
+++ mariadb.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo mysql_secure_installation;
+sudo mysql_secure_installation; 
+sudo mysql --user=root < ./create_phpmyadmin_user.sql;
+
