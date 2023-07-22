ecd9889b9784ce0a6fa808c133d8849b71591286
Randy-jun <275212517@qq.com>
2023-07-22 17:24:26 +0800

    refine

--- autoscript.sh
+++ autoscript.sh
@@ -188,7 +188,7 @@ docker_install()
     sudo install -m 0755 -d /etc/apt/keyrings;
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg;
     echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
-    sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
+    sudo apt update && sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
     sudo docker run hello-world;
     if [ $? = 0 ] ; then
         return 0;
