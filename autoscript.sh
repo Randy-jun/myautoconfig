1ea38651ee334697ee2573594cb3ccd02696d4d2
Randy-jun <275212517@qq.com>
2023-07-22 22:04:52 +0800

    refine

--- autoscript.sh
+++ autoscript.sh
@@ -183,13 +183,15 @@ docker_install()
 {
     echo "Docker current install...";
     # for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt remove $pkg; done
-    sudo apt autoremove docker.io docker-doc docker-compose podman-docker containerd runc;
-    sudo apt -y install ca-certificates curl gnupg;
-    sudo install -m 0755 -d /etc/apt/keyrings;
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg;
-    echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
-    sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
-    sudo docker run hello-world;
+    # sudo apt autoremove docker.io docker-doc docker-compose podman-docker containerd runc;
+    # sudo apt -y install ca-certificates curl gnupg;
+    # sudo install -m 0755 -d /etc/apt/keyrings;
+    # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg;
+    # echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
+    # sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
+    export DOWNLOAD_URL="https://mirrors.tuna.tsinghua.edu.cn/docker-ce";
+    curl -fsSL https://get.docker.com/ | sudo -E sh;
+    # sudo docker run hello-world;
     if [ $? = 0 ] ; then
         return 0;
     else
@@ -312,7 +314,6 @@ main()
     sudo apt update && sudo apt -y full-upgrade;
     sudo apt -y install htop vim tmux zsh curl > /dev/null 2>&1;
     # sudo apt -y install htop vim zsh curl synapse sublime-text > /dev/null 2>&1;
-
     if [ $? = 0 ] ; then
         echo -e "Software has been installed [\033[;32mFinish\033[;m].";
     else
