dbf286ea35839d4ec1f2912e784bd62776f03f01
Randy-jun <2135524+Randy-jun@users.noreply.github.com>
2023-08-31 23:35:05 +0800

    Nodejs

--- autoscript.sh
+++ autoscript.sh
@@ -141,8 +141,11 @@ nodejs_current_install()
     else
         echo "Node.js reinstall...";
     fi
-    # curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash - &&
-    curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash - &&
+    sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg;
+    sudo mkdir -p /etc/apt/keyrings;
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg;
+    NODE_MAJOR=20
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list;
     sudo apt update &&
     sudo apt install nodejs -y &&
     sudo apt install npm -y;
