6176177bc36efbb8e456e0aa87e3b367c20ec2ff
yroot <yroot@debian.debian.local>
2024-08-13 11:52:23 +0800

    add 1panel

--- ip_post.sh
+++ ip_post.sh
@@ -26,6 +26,7 @@ do
           echo "http://[$localipv6]:2283" >> $TEMP
           # echo "http://[$localipv6]:7575" >> $TEMP
           echo "http://[$localipv6]:8123" >> $TEMP
+          echo "http://[$localipv6]:10086" >> $TEMP
           echo "https://[$localipv6]:9443" >> $TEMP
           echo "https://[$localipv6]:9090" >> $TEMP
           mail -s "Debian yroot $(date -R) IP" yangjun.randy@139.com < $TEMP
